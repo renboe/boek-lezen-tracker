fff3cbc0e70251599cead69d48b897e31f0d8eef
renboe <memesm399@gmail.com>
2024-08-06 17:00:40 +0200

    added all possible book data to csv

--- init.py
+++ init.py
@@ -3,13 +3,25 @@ import csv
 def main():
     print("Welkom in de lees tracker!")
     toon_boekenlijst()
+    user_input = input()
+    if (user_input == "mb"):
+        make_book()
 
 def toon_boekenlijst():
     with open('gebruiker_data/boeken.csv') as csvfile:
         reader = csv.reader(csvfile)
         next(reader)
         for row in reader:
-            titel, schrijver, aantal_paginas = row
-            print(f"{titel} van {schrijver} met {aantal_paginas} pagina's")
+            title, author, page_count, isbn, publisher, publication_year, publication_month, publication_day, genre, notes, rating, rating_description = row
+            print(row)
+
+def make_book():
+    title = input("enter book title")
+
+    with open('gebruiker_data/boeken.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)
+        for row in reader:
+            title, author, page_count, isbn, publisher, publication_year, publication_month, publication_day, genre, notes, rating, rating_description = row
 
 main()
